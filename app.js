76fcfbccfd72f5fa0f2c9fa454e79ac4cf1ce202
Thibaut Bongard <thibaut.bongard@gmail.com>
2018-12-08 11:16:47 +0100

    Setting up NodeJs & Express

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    res.redirect('nukes.html');
+});
+
+module.exports = app;
