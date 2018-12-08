864e5548342f43748d9fb2a14f2d93cd60d748ab
Thibaut Bongard <thibaut.bongard@gmail.com>
2018-12-08 11:35:14 +0100

    Setting up Bootstrap

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+// redirect JS jQuery
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+// redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     res.redirect('nukes.html');
